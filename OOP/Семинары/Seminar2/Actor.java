3a6de8f9e552bf5a32a5ffc4e32c351fddeba339
chp_h <veron.grigoreva@gmail.com>
2024-04-15 06:18:15 +0300

    Add abstract class Actor

---
+++ OOP/Семинары/Seminar2/Actor.java
@@ -0,0 +1,11 @@
+package OOP.Семинары.Seminar2;
+
+public abstract class Actor implements ActorBehaviour {
+    protected String name;
+    protected boolean isMakeOrder;
+    protected boolean isTakeOrder;
+
+    public abstract String getName();
+
+    
+}
