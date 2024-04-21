509d6dcc3c2dd0a520f7368be6d1270656769e64
chp_h <veron.grigoreva@gmail.com>
2024-04-21 18:51:07 +0300

    Create User, Student and Teacher

---
+++ OOP/Семинары/Seminar5/Model/Student.java
@@ -0,0 +1,19 @@
+package Семинары.Seminar5.Model;
+
+public class Student extends User{
+    private int studentId;
+
+    public Student(String name, int age, int studentId) {
+        super(name, age);
+        this.studentId = studentId;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+}
---
+++ OOP/Семинары/Seminar5/Model/Teacher.java
@@ -0,0 +1,25 @@
+package Семинары.Seminar5.Model;
+
+public class Teacher extends User{
+    private String disciplesTaught;
+
+    public Teacher(String name, int age, String disciplesTaught) {
+        super(name, age);
+        this.disciplesTaught = disciplesTaught;
+    }
+
+    public String getDisciplesTaught() {
+        return disciplesTaught;
+    }
+
+    public void setDisciplesTaught(String disciplesTaught) {
+        this.disciplesTaught = disciplesTaught;
+    }
+
+    @Override
+    public String toString() {
+        
+        return "преподаватель: '" + name +  
+                "'\nпредмет: " + "'" + disciplesTaught + "'";
+}
+}
