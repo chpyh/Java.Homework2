4ce00ab73d5fa4605f50c876f7a811ffea5e5e2d
chp_h <veron.grigoreva@gmail.com>
2024-04-24 21:53:03 +0300

    Add old project for teacher to check

---
+++ OOP/Семинары/Seminar6/HomeworkSolid/Book.java
@@ -0,0 +1,34 @@
+// package Семинары.Seminar6.HomeworkSolid;
+
+
+// //* Создайте обобщенный класс Book<T>, который представляет книгу в библиотеке. 
+
+// //* Каждая книга должна содержать параметр типа T для ее названия.
+
+// public class Book<T> {
+//    private T name;
+
+//     public Book(T name) {
+//         this.name = name;
+       
+//     }
+
+//     public Book() {
+      
+       
+//     }
+
+//     public T getName() {
+//         return name;
+//     }
+
+//     public void setName(T name) {
+//         this.name = name;
+//     }
+
+//     @Override
+//     public String toString() {
+//         return "\"" + name.toString() + "\"";
+
+//     }
+// }
